9f3e61169754077767c652b328af59bfa4ea0aae
Krushon <email5000@yandex.ru>
2019-07-05 16:07:33 +0300

    Обновлены пути

--- 4_script-new-srv-cert.sh
+++ 4_script-new-srv-cert.sh
@@ -7,7 +7,6 @@ echo "Enter the \"webservername\" (for example: www.example.com)"
 read webservername
 
 ROOTCAPATH=/root/ca
-INTERCAPATH=/root/ca/intermediate
 
 # Создание ключа
 cd $ROOTCAPATH
@@ -15,16 +14,16 @@ cd $ROOTCAPATH
 /bin/chmod 400 intermediate/private/$webservername.key.pem
 
 # Создание сертификата
-## Для серверных сертификатов, Общее Имя (Common Name) должно быть полным доменным именем
-##(fully qualified domain name — FQDN) (к примеру, www.example.com), в то время как для
-## клиентского сертификата оно должно быть любым уникальным идентификатором (к примеру, e-mail
-## адресом). Common Name не может быть таким же, как любой корневой или промежуточный сертификат.
+## Для серверных сертификатов, Общее Имя (Common Name) должно быть полным доменным именем ##(fully qualified domain name — FQDN)
+## (к примеру, www.example.com), в то время как для клиентского сертификата оно должно быть любым уникальным идентификатором
+## (к примеру, e-mail адресом). Common Name не может быть таким же, как любой корневой или промежуточный сертификат.
 cd $ROOTCAPATH
 /usr/bin/openssl req -config intermediate/openssl.cnf -key intermediate/private/$webservername.key.pem -new -sha256 -out intermediate/csr/$webservername.csr.pem
 /usr/bin/openssl ca -config intermediate/openssl.cnf -extensions server_cert -days 375 -notext -md sha256 -in intermediate/csr/$webservername.csr.pem -out intermediate/certs/$webservername.cert.pem
 /bin/chmod 444 intermediate/certs/$webservername.cert.pem
 
 # Проверка цепочки сертификатов
+cd $ROOTCAPATH
 check=`/usr/bin/openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/$webservername.cert.pem`
 if [[ $check = "intermediate/certs/$webservername.cert.pem: OK" ]]; then
     echo -e "\e[32;1mOK\e[0m\e[32m. Сертификат имеет действенную цепочку доверия.\e[0m"
@@ -34,6 +33,7 @@ if [[ $check = "intermediate/certs/$webservername.cert.pem: OK" ]]; then
 fi
 
 # Подготовка комплекта сертификатов и ключа для копирования на web-сервер
+cd $ROOTCAPATH
 /bin/tar -cvf $webservername.tar /root/ca/intermediate/certs/ca-chain.cert.pem /root/ca/intermediate/certs/$webservername.cert.pem /root/ca/intermediate/private/$webservername.key.pem
 echo
 echo "Сертификаты для $webservername подготовлены и упакованы в архив /root/ca/$webservername.tar."
