385a96fefbe50f27435acb14d53e0c4818da1f9b
Krushon <email5000@yandex.ru>
2019-08-27 16:52:18 +0300

    Обновлено до 10

--- 1_script-upgrade.sh
+++ 1_script-upgrade.sh
@@ -3,25 +3,36 @@
 SECONDS=0
 printf "\033c"
 ver=`cat /etc/*-release | grep VERSION_ID`
-#  Если версия 8, то обновляем до 9.
+#  Если версия 8, то обновляем до 10
 if [ $ver = 'VERSION_ID="8"' ]
   then
-    echo "OS version is 8. Upgrading to 9..."
-    sed -i '1,20s/jessie/stretch/' /etc/apt/sources.list
+    echo "OS version is 8. Upgrading to 10..."
+    cp /etc/apt/sources.list /etc/apt/sources.list_backup
+    sed -i 's/jessie/buster/g' /etc/apt/sources.list
     apt-get update
     apt-get upgrade -y
     apt-get dist-upgrade -y
     apt autoremove -y
-#  Если версия 9, то просто обновляем всё.
+#  Если версия 9, то обновляем до 10
 elif [ $ver = 'VERSION_ID="9"' ]
+  then
+    echo "OS version is 9. Upgrading to 10..."
+    cp /etc/apt/sources.list /etc/apt/sources.list_backup
+    sed -i 's/stretch/buster/g' /etc/apt/sources.list
+    apt-get update
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+    apt autoremove -y
+#  Если версия 10, то просто всё обновляем.
+elif [ $ver = 'VERSION_ID="10"' ]
   then
     echo "Updating system..."
     apt-get update
     apt-get upgrade -y
     apt autoremove -y
-# Если версия не 8, не 9 и не debian, то скрипт завершается и потребуется ручное вмешательство.
+# Если версия не 8, не 9, не 10 и не debian, то скрипт завершается и потребуется ручное вмешательство.
   else
-    echo "Что-то пошло не так. Требуется вмешательство."
+    echo -e "\e[31mЧто-то пошло не так. Требуется вмешательство.\e[0m"
     exit 0
 fi
 echo
